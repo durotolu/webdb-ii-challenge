5380a59b9b169771a274e9ba176ea1e2c7bbaee1
Modurotolu Olokode <durolawk@gmail.com>
2019-11-05 15:50:32 +0100

    configured knex to cars.db

--- api/server.js
+++ api/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const carsRouter = require('../cars/cars-router')
+
 const server = express();
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api/cars', carsRouter)
 
 server.get('/', (req, res) => {
     res.send('testing...!!!')
---
+++ cars/cars-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const db = require('../data/db-config')
+console.log(process.env.NODE_ENV);
+
+const router = express.Router();
+
+module.exports = router;
